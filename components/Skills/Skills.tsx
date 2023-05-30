207ee55198a5e55ac82632d88948c088d9486f55
KendallTencio <kendalltencio132e@gmail.com>
2023-05-30 15:30:42 -0600

    Adding Some Skills and SVG

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Skill from "./Skill";
 import { SKILLS_SUB_TITLE, SKILLS_TITLE } from "@/utils/constants";
+import { SKILLS } from "@/utils/skills";
 import "../../styles/Skill.css";
 
 const Skills = () => {
@@ -9,10 +10,9 @@ const Skills = () => {
       <p className={`highlight-text-xl`}>{SKILLS_TITLE}</p>
       <h2 className="default-py">{SKILLS_SUB_TITLE}</h2>
       <div className="skills-grid-container">
-        <Skill skill={"React"} skillSrc={"/../public/assets/React.png"} />
-        <Skill skill={"React"} skillSrc={"/../public/assets/React.png"} />
-        <Skill skill={"React"} skillSrc={"/../public/assets/React.png"} />
-        <Skill skill={"React"} skillSrc={"/../public/assets/React.png"} />
+        {SKILLS.map((skill) => (
+          <Skill skill={skill.name} skillSrc={skill.src} key={skill.id} />
+        ))}
       </div>
     </div>
   );
---
+++ utils/skills.ts
@@ -0,0 +1,6 @@
+export const SKILLS = [
+    {id: 1, name: "React", src: "/assets/React.svg"},
+    {id: 2, name: "Python", src: "/assets/Python.svg"},
+    {id: 3, name: "Javascript", src: "/assets/Javascript.svg"},
+    {id: 4, name: "Unity", src: "/assets/Unity.svg"}
+  ];
